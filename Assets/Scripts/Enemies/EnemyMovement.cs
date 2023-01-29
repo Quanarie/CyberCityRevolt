1aad00d3661196df8ccad750fad8f2dbcc6df031
Quanarie <qustaine@gmail.com>
2023-01-29 01:01:17 +0100

    Enemy path movement added

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Newtonsoft.Json.Bson;
 using UnityEngine;
 
 [RequireComponent(typeof(EnemyInfo))]
@@ -5,10 +7,16 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float minDistanceToOtherEnemies;
+    [SerializeField] private Transform route;
+    [SerializeField] private float waitOnPointTime;
     
     private Rigidbody2D rb;
     private Transform playerTransform;
     private EnemyInfo info;
+    private Vector3[] routePoints;
+    private int currentRoutePoint = 0;
+    private const float MINIMAL_DISTANCE_TO_POINT = 0.1f;
+    private bool isWaiting = false;
 
     private void Start()
     {
@@ -19,31 +27,77 @@ public class EnemyMovement : MonoBehaviour
 
         playerTransform = Singleton.Instance.PlayerData.Player.transform;
         info = GetComponent<EnemyInfo>();
+
+        if (route == null || route.childCount <= 1)
+        {
+            routePoints = new[] { transform.position };
+            return;
+        }
+        
+        routePoints = new Vector3[route.childCount];
+        for (int i = 0; i < route.childCount; i++)
+        {
+            routePoints[i] = route.GetChild(i).position;
+        }
     }
 
     private void FixedUpdate()
     {
+        CollideWithOtherEnemies();
+        
         float distanceToPlayer = Vector3.Distance(playerTransform.position, transform.position);
         
-        // Do not move if enemy is too close or too far away
-        if (distanceToPlayer < info.MinDistanceToPlayer || distanceToPlayer > info.TriggerDistance)
+        if (distanceToPlayer < info.MinDistanceToPlayer || distanceToPlayer > info.TriggerDistance || 
+            info.IsThereObstacleBetweenMeAndPlayer())
         {
-            info.MoveDirection = Vector2.zero;
+            MoveOnRoute();
             return;
         }
+        
+       MoveTo(playerTransform.position);
+    }
 
-        // Stopping enemy when there is an obstacle on the way to player
-        if (!info.IsThereObstacleBetweenMeAndPlayer())
+    private void MoveTo(Vector3 destination)
+    {
+        info.MoveDirection = destination - transform.position;
+        rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * info.MoveDirection.normalized);
+    }
+
+    private void MoveOnRoute()
+    {
+        Vector3 moveToPoint = routePoints[currentRoutePoint];
+        if (Vector3.Distance(moveToPoint, transform.position) < MINIMAL_DISTANCE_TO_POINT)
         {
-            info.MoveDirection = playerTransform.position - transform.position;
-            rb.MovePosition(rb.position + speed * Time.fixedDeltaTime * info.MoveDirection.normalized);
+            if (isWaiting) return;
+            StartCoroutine(WaitAndSetNextPoint());
         }
         else
         {
-            info.MoveDirection = Vector2.zero;
+            MoveTo(moveToPoint);
         }
-        
-        // Collisions with other enemies
+    }
+
+    IEnumerator WaitAndSetNextPoint()
+    {
+        isWaiting = true;
+        info.MoveDirection = Vector2.zero;
+        yield return new WaitForSeconds(waitOnPointTime);
+        isWaiting = false;
+        SetNextRoutePoint();
+    }
+
+    private void SetNextRoutePoint()
+    {
+        if (currentRoutePoint == routePoints.Length - 1)
+        {
+            currentRoutePoint = 0;
+            return;
+        }
+        currentRoutePoint++;
+    }
+
+    private void CollideWithOtherEnemies()
+    {
         foreach (var col in Physics2D.OverlapCircleAll(transform.position, minDistanceToOtherEnemies))
         {
             if (!col.TryGetComponent<EnemyInfo>(out _) || col.gameObject == gameObject) continue;
