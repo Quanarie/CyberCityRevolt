b81f1c17ba6bcd820d32ee246ce115ac748b7a22
Quanarie <qustaine@gmail.com>
2023-02-06 18:31:05 +0100

    Nice output for dialogues, leaving the dialogue when going from interlocutar

--- Assets/Scripts/Dialogue/Talkable.cs
+++ Assets/Scripts/Dialogue/Talkable.cs
@@ -1,29 +1,34 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Talkable : MonoBehaviour
 {
     [SerializeField] private TextAsset dialogue;
+    [SerializeField] private Sprite talkingSprite;
     [SerializeField] private float triggerDistance;
 
-    private string[] _lines;
-    private int _currentLine;
-    private TextMeshProUGUI _text;
-    private PlayerInput _plInput;
+    private string[] lines;
+    private int currentLine;
+    private TextMeshProUGUI text;
+    private Image avatar;
+    
+    private int maxAmountOfSymbolsInLine = 180;
+    private int currentSymbolInLine = 0;
 
     private void Start()
     {
-        _lines = dialogue.text.Split("\n"[0]);
+        lines = dialogue.text.Split("\n"[0]);
 
-        if (_lines.Length == 0)
+        if (lines.Length == 0)
         {
             Debug.LogError("Character: " + gameObject.name + " has empty dialogue file");
         }
         
-        _currentLine = 0;
-        _text = Singleton.Instance.DialogueData.Text;
-        _plInput = Singleton.Instance.PlayerData.Input;
+        currentLine = 0;
+        text = Singleton.Instance.DialogueData.Text;
+        avatar = Singleton.Instance.DialogueData.Avatar;
     }
 
     private void Update()
@@ -31,7 +36,14 @@ public class Talkable : MonoBehaviour
         if (PauseMenuManager.IsPaused) return;
         
         var plPos = Singleton.Instance.PlayerData.Player.transform.position;
-        if (Vector3.Distance(plPos, transform.position) > triggerDistance) return;
+        if (Vector3.Distance(plPos, transform.position) > triggerDistance)
+        {
+            if (Singleton.Instance.DialogueData.IsActive)
+            {
+                HideDialogue();
+            }
+            return;
+        }
 
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
@@ -42,38 +54,58 @@ public class Talkable : MonoBehaviour
             
             if (TryDisplayCurrentLine())
             {
-                _currentLine++;
+                currentLine++;
             }
-        }
-
-        if (Keyboard.current.qKey.wasPressedThisFrame && Singleton.Instance.DialogueData.IsActive)
-        {
-            HideDialogue();
         }
     }
 
     private bool TryDisplayCurrentLine()
     {
-        if (_currentLine < _lines.Length)
+        if (currentLine >= lines.Length)
         {
-            _text.text = _lines[_currentLine];
-            return true;
+            HideDialogue();
+            return false;
+        }
+
+        string lineToOut = "";
+        if (currentSymbolInLine + maxAmountOfSymbolsInLine < lines[currentLine].Length)
+        {
+            int latestSpace = 0;
+
+            for (int i = currentSymbolInLine; i < currentSymbolInLine + maxAmountOfSymbolsInLine; i++)
+            {
+                if (lines[currentLine][i] != ' ') continue;
+
+                latestSpace = i;
+            }
+
+            for (int i = currentSymbolInLine; i < latestSpace; i++)
+            {
+                lineToOut += lines[currentLine][i];
+            }
+            currentSymbolInLine = latestSpace + 1;
+            text.text = lineToOut;
+            return false;
         }
         
-        HideDialogue();
-        return false;
+        for (int i = currentSymbolInLine; i < lines[currentLine].Length; i++)
+        {
+            lineToOut += lines[currentLine][i];
+        }
+        currentSymbolInLine = 0;
+        text.text = lineToOut;
+        avatar.sprite = talkingSprite;
+        return true;
     }
     
     private void DisplayDialogue()
     {
-        _plInput.DeactivateInput();
         Singleton.Instance.DialogueData.DisplayDialogue();
     }
 
     private void HideDialogue()
     {
-        _plInput.ActivateInput();
         Singleton.Instance.DialogueData.HideDialogue();
-        _currentLine = 0;
+        currentLine = 0;
     }
 }
--- Assets/Scripts/Managers/DialogueData.cs
+++ Assets/Scripts/Managers/DialogueData.cs
@@ -1,34 +1,44 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueData : MonoBehaviour
 {
     [field : SerializeField] public GameObject Box { get; private set; }
     [field : SerializeField] public TextMeshProUGUI Text { get; private set; }
+    [field : SerializeField] public Image Avatar { get; private set; }
     
     public bool IsActive { get; private set; }
 
-    private Animator animator;
+    private Animator boxAnimator;
+    private Animator avatarAnimator;
     
     private static readonly int Opened = Animator.StringToHash("Opened");
     
     private void Start()
     {
-        if (!Box.TryGetComponent(out animator))
+        if (!Box.TryGetComponent(out boxAnimator))
         {
             Debug.LogError("No animator on DialogueBox");
+        }
+        
+        if (!Avatar.TryGetComponent(out avatarAnimator))
+        {
+            Debug.LogError("No animator on DialogueAvatar");
         }
     }
 
     public void DisplayDialogue()
     {
         IsActive = true;
-        animator.SetBool(Opened, true);
+        boxAnimator.SetBool(Opened, true);
+        avatarAnimator.SetBool(Opened, true);
     }
 
     public void HideDialogue()
     {
         IsActive = false;
-        animator.SetBool(Opened, false);
+        boxAnimator.SetBool(Opened, false);
+        avatarAnimator.SetBool(Opened, false);
     }
 }
